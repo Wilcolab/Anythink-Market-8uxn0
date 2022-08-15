b8e25b26b7e1f82d185307c475129c07999a5509
Ryan Olsen <ryankolsen@gmail.com>
2022-08-15 14:29:07 -0400

    feat: added query filter called title

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -187,6 +187,18 @@ router.get("/:item", auth.optional, function (req, res, next) {
     })
     .catch(next);
 });
+//Added: return a tag
+router.get("/:tag", auth.optional, function (req, res, next) {
+  Promise.all([
+    req.payload ? User.findById(req.params.id) : null,
+    req.tag.populate("seller").execPopulate(),
+  ])
+    .then(function (resulrs) {
+      let user = results[0];
+      return res.json({ item: req.item.toJSONFor(user) });
+    })
+    .catch(next);
+});
 
 // update item
 // router.put("/:item/:title", auth.required, function (req, res, next) {
