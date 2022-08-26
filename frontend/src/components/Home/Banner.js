b088f0bcd51d88c85eb9db966a41a5680492a9bc
Ryan Olsen <ryankolsen@gmail.com>
2022-08-26 19:29:28 -0400

    feat: added search message

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import { SearchBar } from "./SearchBar";
 
@@ -9,21 +9,37 @@ const Banner = ({
   items,
   filteredItems,
 }) => {
+  const [showSearch, setShowSearch] = useState(false);
+
+  function handleClick() {
+    if (showSearch === false) {
+      setShowSearch(true);
+    }
+  }
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
-        <div className="justify-content-center px-2 row m-auto w-100">
+        <div className="text-decoration-none justify-content-center px-2 row m-auto w-100">
           <span className="px-2" id="get-part">
-            A place to get
+            A place to{""}
+            <span
+              className="text-decoration-none bg-none border-0 "
+              onClick={() => handleClick()}
+            >
+              get
+            </span>
           </span>
-          <SearchBar
-            searchTitle={searchTitle}
-            onChangeSearch={onChangeSearch}
-            onSearch={onSearch}
-            items={items}
-            filteredItems={filteredItems}
-          />
+          {showSearch ? (
+            <SearchBar
+              searchTitle={searchTitle}
+              onChangeSearch={onChangeSearch}
+              onSearch={onSearch}
+              items={items}
+              filteredItems={filteredItems}
+            />
+          ) : null}
           <span className="px-2"> the cool stuff.</span>
         </div>
       </div>
